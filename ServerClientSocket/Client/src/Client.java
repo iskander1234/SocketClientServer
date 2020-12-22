e8b300c222b8f6801ddf55c25e31b570d6224e2c
Iskander Dosgali <idosgali@gmail.com>
2020-12-22 21:37:35 +0600

    Initial commit 2

--- ServerClientSocket/Client/src/Client.java
+++ ServerClientSocket/Client/src/Client.java
@@ -1,20 +1,25 @@
 import dosgali.Phone;
 import java.io.*;
+import java.util.Random;
 
 public class Client {
     public static void main(String[] args) {
+        do {
         try (Phone phone = new Phone("127.0.0.1", 8000)){
-            System.out.println("Connected to server");
 
-            String request = "Almaty";
-            System.out.println("Request: " + request);
-            phone.writeLine(request);
-
-            String response = phone.readLine();
-            System.out.println("Response: " + response);
+                System.out.println("Connected to server");
+                String currentRoom;
+                String[] rooms = {"Almaty", "Astana", "Shymkent", "Taraz", "Aktau", "Aktobe", "Taldyk"};
+                Random rand = new Random();
+                currentRoom = rooms [rand.nextInt( rooms.length)];
+                System.out.println("City : " + currentRoom);
+                phone.writeLine(currentRoom);
+                String response = phone.readLine();
+                System.out.println("City temperature: " + response);
 
         }catch (IOException e){
             e.printStackTrace();
         }
+        }while (true);
     }
 }
--- ServerClientSocket/Server/src/Server.java
+++ ServerClientSocket/Server/src/Server.java
@@ -8,18 +8,21 @@ public class Server {
         try (ServerSocket server = new ServerSocket(8000))
         {
             System.out.println("Server started!");
+            while (true){
+                Phone phone = new Phone(server);
+                    new Thread(() -> {
+                        String request = phone.readLine();
+                        System.out.println("City: " + request);
+                        String response = (int) (Math.random() * 30 - 10) + "";
+                        try {Thread.sleep(4000);} catch (InterruptedException e){ }
+                        phone.writeLine(response);
+                        System.out.println("City temperature: " + response);
+                        try {
+                            phone.close();
+                        } catch (IOException e) {};
+                    }).start();
 
-            while (true)
-                try (Phone phone = new Phone(server)) {
-                    String request = phone.readLine();                         System.out.println("Request: " + request);
-                    String response = (int) (Math.random() * 30 - 10) + "";
-                    Thread.sleep(4000);
-                    phone.writeLine(response);                                 System.out.println("Response: " + response);
                 }
-                catch (NullPointerException | InterruptedException e){
-                    e.printStackTrace();
-                }
-
         }catch (IOException e){
             throw new RuntimeException(e);
         }
